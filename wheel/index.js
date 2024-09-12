0a06a59def441bda3bb019e35f23f14a760119bf
vanyalevel <saintskonnor@gmail.com>
2024-09-13 01:02:09 +0300

    Update index.js

--- wheel/index.js
+++ wheel/index.js
@@ -42,6 +42,21 @@ const dataSets = {
         'Тормоз',
     ],
 
+wheel2: [        
+       'LIMBO',
+       "Mirror's Edge",
+       'Outlast',
+       'Grand Theft Auto: Vice City',
+       'Plants vs. Zombies',
+       'Goat Simulator',
+       'Stray',
+       'Detroit: Become Human',
+       'Blitzkrieg 2 Anthology',
+       'LEGO Star Wars: The Skywalker Saga',
+       'Watch Dogs',
+       "Teenage Mutant Ninja Turtles: Shredder's Revenge",
+    ],
+
     coin: [
         'Орёл',
         'Решка',
